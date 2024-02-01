29c9454741289bb165c1493d21be1a3b3596c5ac
Ricardo Said <ricardo@smartpaymx.com>
2024-02-01 16:05:39 -0600

    adding distribute xamarin

--- AppCenterXamarin/AppCenterXamarin.Droid/MainActivity.cs
+++ AppCenterXamarin/AppCenterXamarin.Droid/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter.Distribute.Android;
 
 namespace AppCenterXamarin.Droid
 {
@@ -19,6 +20,7 @@ namespace AppCenterXamarin.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Distribute.SetEnabledForDebuggableBuild(true);
             LoadApplication(new App());
         }
     }
--- AppCenterXamarin/AppCenterXamarin/App.xaml.cs
+++ AppCenterXamarin/AppCenterXamarin/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using AppCenterXamarin.Views;
 using Microsoft.AppCenter;
+using Microsoft.AppCenter.Distribute;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,9 +21,56 @@ namespace AppCenterXamarin
 
         protected override void OnStart()
         {
+            Distribute.DisableAutomaticCheckForUpdate();
+            Distribute.NoReleaseAvailable = OnNoReleaseAvailable;
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
             AppCenter.Start("android=3aa0c8d9-c0bb-4cae-ad31-194d6f516038;",
                   typeof(Microsoft.AppCenter.Analytics.Analytics),
                   typeof(Microsoft.AppCenter.Crashes.Crashes));
+        }
+        void OnNoReleaseAvailable()
+        {
+            AppCenterLog.Info("AppCenterXamarin", "No release available callback triggered.");
+        }
+        bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            // Look at releaseDetails public properties to get version information, release notes text or release notes URL
+            string versionName = releaseDetails.ShortVersion;
+            string versionCodeOrBuildNumber = releaseDetails.Version;
+            string releaseNotes = releaseDetails.ReleaseNotes;
+            Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
+
+            // custom dialog
+            var title = "Version " + versionName + " available!";
+            Task answer;
+
+            // On mandatory update, user can't postpone
+            if (releaseDetails.MandatoryUpdate)
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
+            }
+            else
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
+            }
+            answer.ContinueWith((task) =>
+            {
+                // If mandatory or if answer was positive
+                if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
+                {
+                    // Notify SDK that user selected update
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
+                }
+                else
+                {
+                    // Notify SDK that user selected postpone (for 1 day)
+                    // This method call is ignored by the SDK if the update is mandatory
+                    Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                }
+            });
+
+            // Return true if you're using your own dialog, false otherwise
+            return true;
         }
 
         protected override void OnSleep()
