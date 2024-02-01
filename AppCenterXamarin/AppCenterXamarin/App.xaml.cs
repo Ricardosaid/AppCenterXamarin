a14002d57e87ccae54b53332f4c1ba2dc17b3f88
Ricardo Said <ricardo@smartpaymx.com>
2024-02-01 16:36:34 -0600

    adding analytics

--- AppCenterXamarin/AppCenterXamarin/App.xaml.cs
+++ AppCenterXamarin/AppCenterXamarin/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using AppCenterXamarin.Views;
 using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Distribute;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -21,12 +22,18 @@ namespace AppCenterXamarin
 
         protected override void OnStart()
         {
+            Analytics.TrackEvent("App started");
             Distribute.DisableAutomaticCheckForUpdate();
+            Analytics.TrackEvent("Disabling automatic check for updates");
             Distribute.NoReleaseAvailable = OnNoReleaseAvailable;
+            Analytics.TrackEvent("Setting no release available callback");
             Distribute.ReleaseAvailable = OnReleaseAvailable;
+            Analytics.TrackEvent("Setting release available callback");
+            
             AppCenter.Start("android=3aa0c8d9-c0bb-4cae-ad31-194d6f516038;",
                   typeof(Microsoft.AppCenter.Analytics.Analytics),
                   typeof(Microsoft.AppCenter.Crashes.Crashes));
+            Analytics.TrackEvent("AppCenter started");
         }
         void OnNoReleaseAvailable()
         {
@@ -36,9 +43,13 @@ namespace AppCenterXamarin
         {
             // Look at releaseDetails public properties to get version information, release notes text or release notes URL
             string versionName = releaseDetails.ShortVersion;
+            Analytics.TrackEvent("Release available for version " + versionName);
             string versionCodeOrBuildNumber = releaseDetails.Version;
+            Analytics.TrackEvent("Release available for version code " + versionCodeOrBuildNumber);
             string releaseNotes = releaseDetails.ReleaseNotes;
+            Analytics.TrackEvent("Release notes: " + releaseNotes);
             Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
+            Analytics.TrackEvent("Release notes URL: " + releaseNotesUrl);
 
             // custom dialog
             var title = "Version " + versionName + " available!";
@@ -47,10 +58,12 @@ namespace AppCenterXamarin
             // On mandatory update, user can't postpone
             if (releaseDetails.MandatoryUpdate)
             {
+                Analytics.TrackEvent("Mandatory update, user can't postpone");
                 answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
             }
             else
             {
+                Analytics.TrackEvent("Not mandatory update, user can postpone");
                 answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
             }
             answer.ContinueWith((task) =>
@@ -68,7 +81,7 @@ namespace AppCenterXamarin
                     Distribute.NotifyUpdateAction(UpdateAction.Postpone);
                 }
             });
-
+            Analytics.TrackEvent("Returning true");
             // Return true if you're using your own dialog, false otherwise
             return true;
         }
